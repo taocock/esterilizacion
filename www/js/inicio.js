5b110c49f7a5f796e86a717c6a1a65468da4014b
taocock <41911829+taocock@users.noreply.github.com>
2018-08-08 12:37:16 +0100

    Add files via upload

--- www/js/inicio.js
+++ www/js/inicio.js
@@ -1,10 +1,25 @@
 
 		var app = angular.module('myApp', []);
-		app.controller('myCtrl', function($scope) {
+		app.controller('myCtrl', function($scope,$http) {
+
     	$scope.name = "John Doe";
     	$scope.test = function(){
     		$scope.name = "echo click test";
-    	}
+    		var url = "http://10.138.50.47/esterilizacion/indexapk.php/apk/getServicios";
+  	    $http({method: 'GET', url: url}
+        ).success(function(data, status, headers, config) {
+
+          //callback(data);        
+          alert(data);
+        }).
+        error(function(data, status, headers, config) {
+          alert("status :" + status);          
+          alert("error");    
+
+        });      		
+
+    	};
+
 		});
 
 
