ef7268680bae2dcdd4cc9cb5fa26aa60bc3376f4
taocock <41911829+taocock@users.noreply.github.com>
2018-08-08 12:20:58 +0100

    Add files via upload

--- www/js/inicio.js
+++ www/js/inicio.js
@@ -2,6 +2,11 @@
 		var app = angular.module('myApp', []);
 		app.controller('myCtrl', function($scope) {
     	$scope.name = "John Doe";
+    	$scope.test = function(){
+    		$scope.name = "echo click test";
+    	}
 		});
 
-		alert("fin");
+
+
+		alert("fin");  
